61799326f3947a765a748190370e4d4ffbf80fb8
Dmitry <DmitryVoln@mail.ru>
2021-07-09 17:53:42 +0300

    task1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" style="width: 100px; height: 100px; margin: auto">
+    
+    Welcome to: {{calculator}}
+    <Calc/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Calc from './components/Calc.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Calc,
+  },
+  data: () => ({
+    calculator: 'calculator'
+  })
 }
 </script>
 
 <style lang="scss">
 #app {
+  display: flex;
+  flex-direction: column;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  width: 50px;
+  margin:  auto;
+}
+.calc {
+  display: flex;
+  justify-content: space-around;
+}
+.result {
+  border: solid 1px #000;
+  width: 100px;
 }
 </style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="calc">
+            <input type="number" placeholder="op1" v-model.number="operand1" class="input">
+            <input type="number" placeholder="op2" v-model.number="operand2" class="input">
+            <div class="result"> = {{ result }}</div>
+        </div>
+        <button v-on:click="result = operand1 + operand2">+</button>
+        <button v-on:click="result = operand1 - operand2">-</button>
+        <button v-on:click="result = operand1 * operand2">*</button>
+        <button v-on:click="result = operand1 / operand2">/</button>
+        <button v-on:click="result = Math.pow(operand1, operand2)">степень</button>
+        <button v-on:click="result = Math.floor(operand1 / operand2)">цел.дел.</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Calc",
+    data: () => ({
+        operand1: null,
+        operand2: null,
+        result: 0,
+    })
+}
+</script>
